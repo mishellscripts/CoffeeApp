8a0b89434b26ea5e2d387c1879ac726f566b90f9
Michelle <Michelle@Lenovo-Labtop>
2016-12-28 11:56:34 -0800

    Testing

--- src/CoffeeDemo.java
+++ src/CoffeeDemo.java
@@ -3,5 +3,8 @@ public class CoffeeDemo {
 	public static void main(String[] args) {
 		CoffeeModel model = new CoffeeModel();
 		SignIn signIn = new SignIn(model);
+		
+		
+		//comment here test
 	}
 }
